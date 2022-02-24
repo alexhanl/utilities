6ddb706fe416ab17e813d8b875b08756344e3576
hanliang <liang.han@gmail.com>
2022-02-24 10:46:58 +0800

    change the wcp namespace

--- onecloud-demo-env/login-tkg-demo.sh
+++ onecloud-demo-env/login-tkg-demo.sh
@@ -1,2 +1,2 @@
-kubectl vsphere login --server wcp.corp.tanzu -u administrator@vsphere.local --tanzu-kubernetes-cluster-namespace tkg --tanzu-kubernetes-cluster-name tkg-demo
+kubectl vsphere login --server wcp.corp.tanzu -u administrator@vsphere.local --tanzu-kubernetes-cluster-namespace marketing --tanzu-kubernetes-cluster-name tkg-demo
 kubectl config use-context tkg-demo

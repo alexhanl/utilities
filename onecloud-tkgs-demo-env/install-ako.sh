4904ee434f4a20cb8cb905672cfeae1e255265a2
hanliang <liang.han@gmail.com>
2022-03-11 13:43:37 +0800

    ako 1.6.1

--- onecloud-tkgs-demo-env/install-ako.sh
+++ onecloud-tkgs-demo-env/install-ako.sh
@@ -1,3 +1,4 @@
 kubectl create ns avi-system
 helm repo add ako https://projects.registry.vmware.com/chartrepo/ako
-helm install ako-1.5.2 ako/ako --values ./ako-1.5.2-values.yaml --namespace=avi-system --version 1.5.2
+# helm install ako-1.5.2 ako/ako --values ./ako-1.5.2-values.yaml --namespace=avi-system --version 1.5.2
+helm install ako-1.6.1 ako/ako --values ./ako-1.6.1-values.yaml --namespace=avi-system --version 1.6.1

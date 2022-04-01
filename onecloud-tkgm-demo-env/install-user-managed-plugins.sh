5a8dddddb1bd2fefcd70fe8d6add5bbfcfb85dd9
hanliang <liang.han@gmail.com>
2022-04-01 09:08:45 +0800

    more spaces

--- onecloud-tkgm-demo-env/install-user-managed-plugins.sh
+++ onecloud-tkgm-demo-env/install-user-managed-plugins.sh
@@ -3,13 +3,13 @@
 
 kubectl create ns my-packages
 
-echo "--- installing cert-manager ---"
+echo "\n\n\n--- installing cert-manager ---"
 tanzu package install cert-manager \
 --package-name cert-manager.tanzu.vmware.com \
 --namespace my-packages \
 --version 1.5.3+vmware.2-tkg.1 
 
-echo "--- installing contour ---"
+echo "\n\n\n--- installing contour ---"
 kubectl create clusterrolebinding envoy-tkg-admin-privileged-binding \
 --clusterrole=psp:vmware-system-privileged \
 --serviceaccount=tanzu-system-ingress:envoy
@@ -20,7 +20,7 @@ tanzu package install contour \
 --values-file contour-data-values.yaml \
 --namespace my-packages
 
-echo "--- installing external-dns ---"
+echo "\n\n\n--- installing external-dns ---"
 tanzu package install external-dns \
 --package-name external-dns.tanzu.vmware.com \
 --version 0.10.0+vmware.1-tkg.1 \
